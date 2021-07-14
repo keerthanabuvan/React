eec35c02f2bda7db83a26ce89274245440a882c3
Keerthana <keerthanabuvan@gmail.com>
2021-07-13 21:07:34 -0700

    Day 1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react"
+import ReactDom from "react-dom"
+import "./style.css"
+import Button from "./Button.js"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const Apple=()=>{
+    return(
+        <div>
+            <h1 style={{textAlign:"center"}}>Hello world</h1>
+            <h2>Hello youtubers</h2>
+            <Button buttonText="Apple"/>
+            <Button buttonText="Orange"/>
+            <Button buttonText="Grapes"/>
+
+        </div>
+    )
 }
+ReactDom.render(<Apple/>,document.getElementById("root"))
+export default Apple
 
-export default App;
+
